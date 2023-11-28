30b78a9a497d15bea1d38eb680889212a6d0f6a0
Nur Hamid <nurhamidqq@gmail.com>
2023-11-28 13:30:45 +0700

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_bloc_fakestoreapi
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/github/nurhamidqq/flutter_bloc_fakestoreapi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.nurhamidqq.flutter_bloc_fakestoreapi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
